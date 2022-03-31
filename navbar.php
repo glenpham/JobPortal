0492f7d666929218db3b6764460ae2a586f73d08
Julyssaur <julissa.yan@hotmail.com>
2022-03-31 19:26:25 -0400

    updated

--- navbar.php
+++ navbar.php
@@ -30,7 +30,8 @@
     <div class="navbar">
       <a class="active">JOB PORTAL</a>
       <a href="/JobPortal/Company/postingPosition.php">Job Posting</a>
-      <a href="/JobPortal/Company/DashboardJobPosting/dashboard.php">View Job Activity</a>
+      <a href="/JobPortal/Company/DashboardJobPosting/dashboard.php">View Jobs Posted</a>
+      <a href="/JobPortal/Company/DashboardJobActivity/jobActivity.php">View Job Activity</a>
       <a href="/JobPortal/index.php" class="right">Logout</a>
     </div>
 <?php  
